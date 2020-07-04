fe9efc238518a4db269fe72797c74bb51c175eff
Jahred Hope <jahredhope@gmail.com>
2020-07-04 10:59:40 +1000

    Add 3rd Extends test

---
+++ test/e2e/extends.3a.ts
@@ -0,0 +1,70 @@
+export const input = {
+  title: 'AnyShape',
+  oneOf: [
+    {
+      $ref: '#/definitions/circle'
+    },
+    {
+      $ref: '#/definitions/square'
+    }
+  ],
+  definitions: {
+    shape: {
+      title: 'Shape',
+      properties: {
+        name: {type: 'string'},
+        type: {type: 'string'}
+      },
+      additionalProperties: false,
+      required: ['name', 'type']
+    },
+    circle: {
+      title: 'Circle',
+      type: 'object',
+      extends: [
+        {
+          $ref: '#/definitions/shape'
+        }
+      ],
+      properties: {
+        type: {
+          type: 'string',
+          enum: ['circle']
+        },
+        radius: {
+          type: 'number'
+        }
+      },
+      required: ['type', 'radius'],
+      additionalProperties: false
+    },
+    square: {
+      title: 'Square',
+      type: 'object',
+      extends: [
+        {
+          $ref: '#/definitions/shape'
+        }
+      ],
+      properties: {
+        type: {
+          type: 'string',
+          enum: ['square']
+        },
+        height: {
+          type: 'number'
+        },
+        width: {
+          type: 'number'
+        }
+      },
+      required: ['type', 'height', 'width'],
+      additionalProperties: false
+    }
+  },
+  additionalProperties: false
+}
+
+export const options = {
+  declareExternallyReferenced: true
+}
